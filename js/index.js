232d3d706c05a65c064ea926d81b0eb61fdfbdda
AIndoria <indoriaabhishek@gmail.com>
2018-07-09 00:58:41 -0700

    Add Functional Navbar

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 let toggle=0;
-
+let navToggle=document.getElementById("nav");
 function navClick(){
   toggle+=1;
-  let navToggle=document.getElementById("nav");
-  navToggle.classList.toggle("nav-expanded");
-  navToggle.classList.toggle(".nav:hover");
+  navToggle.classList.add("nav-expanded");
+  document.getElementById("nav-list").style.display="flex";
 }
 
 document.addEventListener("click", function(event){
-  let navToggle=document.getElementById("nav");
-  if(event.target.closest(".nav")) navClick();
+  if(event.target.closest(".nav") && !navToggle.matches('.nav-expanded')) navClick();
   else{
+    document.getElementById("nav-list").style.display="none";
     navToggle.classList.remove("nav-expanded");
     navToggle.classList.add("nav");
   }
 });
+
